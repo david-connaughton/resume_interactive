caee58479da2011bf939e36af827ae4cad49df0c
Ubuntu <ubuntu@ip-172-31-83-213.ec2.internal>
2019-06-27 01:06:29 +0000

    Mail JS Added

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,13 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "project_request": contactForm.projectsummary.value
+        })
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+        }, function(error) {
+            console.log('FAILED...', error);
+        });
+    return false;
+}
